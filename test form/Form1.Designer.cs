d5dfec75389c607369e3a3e0e01c2e623a8d8426
Shadow320710 <imadurasaputhar@gmail.com>
2024-12-26 15:30:43 +0530

    LAble added

--- test form/Form1.Designer.cs
+++ test form/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.houseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.horopathanaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -84,11 +85,22 @@
             this.horopathanaToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
             this.horopathanaToolStripMenuItem.Text = "Horopathana";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(152, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Imadu";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -110,6 +122,7 @@
         private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem houseToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem horopathanaToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- test form/Form1.cs
+++ test form/Form1.cs
@@ -28,5 +28,10 @@ namespace test_form
         {
             
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
